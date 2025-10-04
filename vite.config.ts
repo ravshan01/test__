00a55c8ad00acc9c0231b718cc5fa2cd6de12a4a
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2025-10-04 21:26:57 +0500

    add '@' path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
