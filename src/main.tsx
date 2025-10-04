8e7e6644d6523775a20bfa630022101b07f4aeaf
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2025-10-04 21:22:00 +0500

    call IOC.init in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './app/components/App'
+import { IOC } from './ioc/ioc.ts'
+
+IOC.init()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
