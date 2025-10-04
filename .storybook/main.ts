039cf3520a8ee05a03aac1b47c09777afbe28d08
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2025-10-04 23:18:05 +0500

    update .storybook/main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,15 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
-};
-export default config;
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
+}
+export default config
