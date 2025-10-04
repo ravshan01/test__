20ea0c3af886ebb4be09dd9e55ab619e68fa759c
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2025-10-04 21:24:23 +0500

    main.tsx import '@ant-design/v5-patch-for-react-19'

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import '@ant-design/v5-patch-for-react-19'
+
 import { App } from './app/components/App'
 import { IOC } from './ioc/ioc.ts'
 
