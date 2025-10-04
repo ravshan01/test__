9b851401f3dac2e180879257cee2a4f7b058f513
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2025-10-04 23:07:08 +0500

    fix eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -18,7 +18,7 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
-      s,
+      storybook.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
