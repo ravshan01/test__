d581def290b9c9e12bebf8594455d23930abe82a
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2025-10-05 16:05:19 +0500

    create AuthFormCardSubHeader

---
+++ src/auth/components/AuthFormCard/components/AuthFormCardSubHeader/AuthFormCardSubHeader.tsx
@@ -0,0 +1,31 @@
+import cn from 'classnames'
+import type { HTMLAttributes } from 'react'
+
+import { Typography } from 'antd'
+
+import { useAuthFormCardTitleDescription } from '@/auth/components/AuthFormCard/hooks/use-auth-form-card-title-description.hook.ts'
+
+import styles from './AuthFormCardSubHeader.module.sass'
+
+export function AuthFormCardSubHeader(props: IAuthFormCardSubHeaderProps) {
+  const { className, step, ...rest } = props
+  const rootClass = cn(styles.root, className)
+
+  const { title, description } = useAuthFormCardTitleDescription(step)
+
+  return (
+    <div className={rootClass} {...rest}>
+      <Typography.Title className={styles.subHeader__title} level={3}>
+        {title}
+      </Typography.Title>
+      <Typography.Text className={styles.subHeader__description}>
+        {description}
+      </Typography.Text>
+    </div>
+  )
+}
+
+export interface IAuthFormCardSubHeaderProps
+  extends HTMLAttributes<HTMLDivElement> {
+  step: 'login' | '2fa'
+}
